57e1972d765f44b98d0f99c1fde7978e7e52bd9d
Nishad Sheikh <nishadsheikh714@gmail.com>
2026-07-14 00:55:19 +0530

    Add files via upload

---
+++ Zomato_Controller.java
@@ -0,0 +1,12 @@
+package com.tka.controller;
+
+import com.tka.service.Zomato_service;
+
+public class Zomato_Controller {
+
+    public static void main(String[] args) {
+
+        Zomato_service service = new Zomato_service();
+        service.serviceMethod();
+    }
+}
